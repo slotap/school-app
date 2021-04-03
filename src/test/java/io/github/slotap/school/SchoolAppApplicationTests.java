2770ccfd1d197a6ceeaec4a59044e8359a30676d
slotap <slotap@gmail.com>
2021-04-03 10:14:01 +0200

    School App project created

---
+++ src/test/java/io/github/slotap/school/SchoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.slotap.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
